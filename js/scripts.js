1f3fc7443eb28e16971139c31b9581c6ace6daed
Norma <kamacho_24@eagles.ewu.edu>
2018-10-30 11:39:52 -0700

    Successful weather retrieval

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-console.log( $() );
 
-console.log('ok');
+
+$.simpleWeather({
+    location: 11722,
+    success: function(weather) {
+      console.log(weather.image);
+      console.log(weather.city);
+      console.log(weather.temp);
+      console.log(weather.updated);
+
+      $('.image img').attr('src',weather.image);
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('.time').text(weather.updated);
+    }, 
+    error: function(error) {
+      console.log('Go outside!');
+    }
+ });
